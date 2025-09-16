da0e0cc237e2c54a705b892d0447403b486545fd
Christopher Bartz <christopher.bartz@canonical.com>
2025-09-16 14:10:45 +0200

    use env vars in main

--- webhook-gateway/main.go
+++ webhook-gateway/main.go
@@ -8,21 +8,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/canonical/mayfly/internal/queue"
 	"github.com/canonical/mayfly/internal/webhook"
 )
 
+const queueName = "webhook-queue"
+const webhookPath = "/webhook"
+
 func main() {
-	uri := "amqp://guest:guest@localhost:5672/"
-	queueName := "webhook-queue"
+
+	port, found := os.LookupEnv("APP_PORT")
+	if !found {
+		log.Panicf("APP_PORT environment variable not set")
+	}
+	uri, found := os.LookupEnv("RABBITMQ_CONNECT_STRING")
+	if !found {
+		log.Panicf("RABBITMQ_CONNECT_STRING environment variable not set")
+	}
+	webhookSecret, found := os.LookupEnv("WEBHOOK_SECRET")
+	if !found {
+		log.Panicf("WEBHOOK_SECRET environment variable not set")
+	}
 
 	q := queue.NewAmqpQueue(uri, queueName)
 	q.StartProducer()
 
 	webhook.InitQueue(q)
-	webhook.InitWebhookSecret("fake-secret")
+	webhook.InitWebhookSecret(webhookSecret)
 
-	http.HandleFunc("/webhook", webhook.WebhookHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc(webhookPath, webhook.WebhookHandler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
