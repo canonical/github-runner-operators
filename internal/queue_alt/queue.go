60addafd2cbf0cd8b11532ece24284540b060bf7
Christopher Bartz <christopher.bartz@canonical.com>
2025-09-22 12:55:37 +0200

    add queue declare and connection reset logic

--- internal/queue_alt/queue.go
+++ internal/queue_alt/queue.go
@@ -5,21 +5,28 @@
 
 package queue_alt
 
-import amqp "github.com/rabbitmq/amqp091-go"
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 type AmqpChannel interface {
 	PublishWithDeferredConfirm(exchange string, key string, mandatory, immediate bool, msg amqp.Publishing) (*interface{}, error)
 	IsClosed() bool
+	Confirm(noWait bool) error
+	QueueDeclare(name string, durable, autoDelete, exclusive, noWait bool, args amqp.Table) (amqp.Queue, error)
 }
 
 type AmqpConnection interface {
 	Channel() (AmqpChannel, error)
+	IsClosed() bool
 }
 
 type AmqpProducer struct {
 	amqpChannel    AmqpChannel
 	amqpConnection AmqpConnection
-	// Add fields for AMQP connection, channel, etc.
+	connectFunc    func(uri string) (AmqpConnection, error)
+	uri            string
+	name           string
 }
 
 type Producer interface {
@@ -27,6 +34,10 @@ type Producer interface {
 }
 
 func (p *AmqpProducer) Push(ctx interface{}, headers map[string]interface{}, msg []byte) error {
+	if p.amqpConnection == nil || p.amqpConnection.IsClosed() {
+		p.resetConnection()
+	}
+
 	if p.amqpChannel == nil || p.amqpChannel.IsClosed() {
 		p.resetChannel()
 	}
@@ -44,8 +55,25 @@ func (p *AmqpProducer) Push(ctx interface{}, headers map[string]interface{}, msg
 	return nil
 }
 
+func (p *AmqpProducer) resetConnection() error {
+	conn, _ := p.connectFunc(p.uri)
+	p.amqpConnection = conn
+	return nil
+}
+
 func (p *AmqpProducer) resetChannel() error {
 	c, _ := p.amqpConnection.Channel()
 	p.amqpChannel = c
+
+	c.Confirm(false)
+
+	c.QueueDeclare(
+		p.name, // name
+		true,   // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
+	)
 	return nil
 }
--- internal/queue_alt/queue_test.go
+++ internal/queue_alt/queue_test.go
@@ -13,9 +13,12 @@ import (
 )
 
 type MockAmqpChannel struct {
-	msgs     [][]byte
-	headers  []map[string]interface{}
-	isclosed bool
+	msgs         [][]byte
+	headers      []map[string]interface{}
+	isclosed     bool
+	confirmMode  bool
+	queueName    string
+	queueDurable bool
 }
 
 func (ch *MockAmqpChannel) PublishWithDeferredConfirm(_ string, _ string, _, _ bool, msg amqp.Publishing) (*interface{}, error) {
@@ -29,15 +32,31 @@ func (ch *MockAmqpChannel) IsClosed() bool {
 	return ch.isclosed
 }
 
+func (ch *MockAmqpChannel) Confirm(_ bool) error {
+	ch.confirmMode = true
+	return nil
+}
+
+func (ch *MockAmqpChannel) QueueDeclare(name string, durable, _, _, _ bool, _ amqp.Table) (amqp.Queue, error) {
+	ch.queueName = name
+	ch.queueDurable = durable
+	return amqp.Queue{}, nil
+}
+
 type MockAmqpConnection struct {
 	channelCalls int
 	amqpChannel  *MockAmqpChannel
+	isclosed     bool
 }
 
 func (m *MockAmqpConnection) Channel() (AmqpChannel, error) {
 	m.channelCalls++
 	m.amqpChannel = &MockAmqpChannel{}
 	return m.amqpChannel, nil
+}
+
+func (m *MockAmqpConnection) IsClosed() bool {
+	return m.isclosed
 }
 
 func TestPush(t *testing.T) {
@@ -49,6 +68,9 @@ func TestPush(t *testing.T) {
 	mockAmqpChannel := &MockAmqpChannel{}
 	amqpProducer := &AmqpProducer{
 		amqpChannel: mockAmqpChannel,
+		amqpConnection: &MockAmqpConnection{
+			amqpChannel: mockAmqpChannel,
+		},
 	}
 
 	headers := map[string]interface{}{"header1": "value1"}
@@ -92,5 +114,66 @@ func TestPushNoChannel(t *testing.T) {
 			assert.Contains(t, mockAmqpConnection.amqpChannel.msgs, []byte("TestMessage"), "expected message to be published")
 		})
 	}
+}
 
+func TestPushNoConnection(t *testing.T) {
+	/*
+		arrange: create a queue with no amqp connection
+		act: push a message to the queue
+		assert: connection got established and message was published to the amqp channel
+	*/
+	tests := []struct {
+		name       string
+		connection AmqpConnection
+	}{
+		{
+			name:       "connection is nil",
+			connection: nil,
+		},
+		{
+			name: "connection is closed",
+			connection: &MockAmqpConnection{
+				isclosed: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			amqpConnection := &MockAmqpConnection{
+				amqpChannel: &MockAmqpChannel{},
+			}
+			amqpProducer := &AmqpProducer{
+				amqpConnection: tt.connection,
+				uri:            "amqp://guest:guest@localhost:5672/",
+				connectFunc: func(uri string) (AmqpConnection, error) {
+					return amqpConnection, nil
+				},
+			}
+
+			amqpProducer.Push(nil, nil, []byte("TestMessage"))
+			assert.Equal(t, 1, amqpConnection.channelCalls, "expected connection to be re-established")
+			assert.Contains(t, amqpConnection.amqpChannel.msgs, []byte("TestMessage"), "expected message to be published")
+		})
+	}
+}
+
+func TestPushQueueDeclare(t *testing.T) {
+	/*
+		arrange: create a queue with no amqp channel
+		act: push a message to the queue
+		assert: channel with confirm mode is established and queue is declared
+	*/
+	mockAmqpConnection := &MockAmqpConnection{}
+	queueName := "fakeQueue"
+	amqpProducer := &AmqpProducer{
+		amqpChannel:    nil,
+		amqpConnection: mockAmqpConnection,
+		name:           queueName,
+	}
+
+	amqpProducer.Push(nil, nil, []byte("TestMessage"))
+
+	assert.Equal(t, mockAmqpConnection.amqpChannel.confirmMode, true, "expected channel to be in confirm mode")
+	assert.Equal(t, mockAmqpConnection.amqpChannel.queueName, queueName, "expected queue name to be "+queueName)
+	assert.Equal(t, mockAmqpConnection.amqpChannel.queueDurable, true, "expected queue to be durable")
 }
