25dd11772df67202b128797ad83389fb65cf95eb
Christopher Bartz <christopher.bartz@canonical.com>
2025-10-01 07:40:39 +0200

    use pre-run-script

--- build-webhook-gateway-rock.sh
+++ build-webhook-gateway-rock.sh
@@ -7,9 +7,7 @@
 
 export ROCKCRAFT_ENABLE_EXPERIMENTAL_EXTENSIONS=true
 
-mv ./rockcraft.yaml /tmp/rockcraft.yaml.bak
 ln -s ./webhook-gateway-rockcraft.yaml ./rockcraft.yaml
 rockcraft clean
 rockcraft pack
 rm ./rockcraft.yaml
-mv /tmp/rockcraft.yaml.bak ./rockcraft.yaml
