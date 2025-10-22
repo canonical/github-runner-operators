7fa5fcbe86f9ce9c9fc5b105e193e60d6ab706e3
florentianayuwono <florentiana.yuwono@canonical.com>
2025-10-22 15:35:05 +0700

    add vault providers

--- webhook-gateway/charm/terraform/versions.tf
+++ webhook-gateway/charm/terraform/versions.tf
@@ -2,7 +2,13 @@
 # See LICENSE file for licensing details.
 
 terraform {
+  required_version = ">= 1.6.6"
   required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.3.0"
+    }
+
     juju = {
       source  = "juju/juju"
       version = ">= 0.23.0"
