b1a1dccb1a58c5d9f2ee2e12bac0a4288f6e856d
florentianayuwono <florentiana.yuwono@canonical.com>
2026-01-21 18:44:54 +0700

    shorten comment

--- internal/planner/server.go
+++ internal/planner/server.go
@@ -445,7 +445,7 @@ type updateJobRequest struct {
 }
 
 // updateJob handles updating a job's started_at and/or completed_at fields that are not yet set.
-// Note: This operation is not atomic. Concurrent updates to the same job may race.
+// This operation is not atomic. Concurrent updates to the same job may race.
 // This is acceptable as the endpoint is intended for occasional debug use.
 func (s *Server) updateJob(w http.ResponseWriter, r *http.Request) {
 	platform := r.PathValue("platform")
