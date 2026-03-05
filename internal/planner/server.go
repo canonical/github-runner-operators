6099760d339210fb8dbf45a82f6682174dba11e9
Christopher Bartz <christopher.bartz@canonical.com>
2026-03-05 07:34:00 +0100

    feat: add logging handler to the planner server (#136)
    * feat: add logging handler to the planner server
    
    * move to dedicated package
    
    * address review

--- internal/planner/server.go
+++ internal/planner/server.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/canonical/github-runner-operators/internal/database"
+	"github.com/canonical/github-runner-operators/internal/server"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
@@ -69,6 +70,7 @@ type AuthStore interface {
 type Server struct {
 	metrics    *Metrics
 	mux        *http.ServeMux
+	handler    http.Handler
 	store      FlavorStore
 	auth       AuthStore
 	adminToken string
@@ -94,13 +96,14 @@ func NewServer(store FlavorStore, auth AuthStore, metrics *Metrics, adminToken s
 	s.mux.Handle("GET "+jobPattern, otelhttp.WithRouteTag(jobPattern, s.tokenProtected(http.HandlerFunc(s.getJob))))
 	s.mux.Handle("PATCH "+jobPattern, otelhttp.WithRouteTag(jobPattern, s.tokenProtected(http.HandlerFunc(s.updateJob))))
 	s.mux.Handle("GET "+healthPattern, otelhttp.WithRouteTag(healthPattern, http.HandlerFunc(s.health)))
+	s.handler = server.LoggingHandler(logger, s.mux)
 
 	return s
 }
 
 // ServeHTTP routes incoming HTTP requests to the appropriate handler methods.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s.mux.ServeHTTP(w, r)
+	s.handler.ServeHTTP(w, r)
 }
 
 // flavorRequest represents the expected JSON payload for creating a flavor.
