5b60254b0a2701e86eb103903c1e1ea3c598856e
Christopher Bartz <christopher.bartz@canonical.com>
2026-06-26 12:43:20 +0000

    test(garm): reuse the shared RSA test key in test_garm
    Drop test_garm.py's local copy of the throwaway RSA key and import the
    TEST_RSA_PRIVATE_KEY constant from tests.integration.helpers, so there is a
    single definition to maintain (and for secret scanners to allowlist).

--- charms/tests/integration/test_garm.py
+++ charms/tests/integration/test_garm.py
@@ -22,6 +22,8 @@ from tenacity import (
 )
 from urllib3.util.retry import Retry
 
+from tests.integration.helpers import TEST_RSA_PRIVATE_KEY as _TEST_RSA_PRIVATE_KEY
+
 logger = logging.getLogger(__name__)
 
 GARM_BINARY = "/usr/local/bin/garm"
@@ -41,22 +43,6 @@ _SCALESET_TEST_CREDENTIAL_NAME = "github-app-12345"
 # Credential name the GARM charm derives from the configurator's github-app-id +
 # installation id (12345 / 67890).
 _SYNCED_CREDENTIAL_NAME = "app-12345-67890"
-
-_TEST_RSA_PRIVATE_KEY = """-----BEGIN RSA PRIVATE KEY-----
-MIICXwIBAAKBgQC2tCW5B18y5VnqqokOeamJgasI3H1405WWv7FmWl31I1Cgabhi
-MFcHdNECXFUC3wtqo/bXyCQbANRBkpZudJfSGos3+1iOJK1fd+MU8ntHVtgpvb5j
-whdFSVJ9EL4/2u0K0S+fIyilD9q7K5mhk0MYLYWumPIRLkbtwr9a7LgY5wIDAQAB
-AoGBAKIQGCoRjPNjmCfdT6fEaYtstt8sXiwQWu+WaHDnFdL9mWZBgOmwAXK+vyt9
-5XafjMvyV2I+yTAewyjLM58U0xlslJu6Bk0Zw920sTmK9Qvvq/2mjsqw+PWr9rRx
-qZFDCefAlB0Npo9tXHAf3ec5+vlm4QsEl6dty+Wx6aSHHMRpAkEA8e5IwkJZFcWO
-aCc8Z+cnoidomlkvGlruncXMG1KhisQTleQVc1bM8tIZq2nNUG1zKJqHeCacQLiV
-LKALnZDSCwJBAMFUIHd7ikYaAgTvrAKmzOZlMKVuGr2SHPODWoaWkEagEsrOw+H2
-PYonSYkbzPyXH6iKUOhWH+ZA1r6K1lhdWhUCQQCquaTOsVN8cbVU+ps+F3l4jKbc
-hSMgThsla3flsCIfcs7/b71Tb2Wh1XIX7Mnef95MQQBoYZbSdW+P1kFcJ96RAkEA
-oSyuqI4BGDJkjpL1l3xSBJ5F8RUbDAI9SrKujNgHTinzoMrCOabdZUkdoEXiHo8r
-IIq3qwrqKz7RCSecTSz+hQJBAJDKODanbnrPxNDgmIp52BMtiYI4vv7gKp/MSW0N
-PG8an+PHNVGDEj1cOOwp/YNQieRp/WPH6bpBtwwe0r6pQZQ=
------END RSA PRIVATE KEY-----"""
 
 
 def test_garm_blocks_without_postgresql(
